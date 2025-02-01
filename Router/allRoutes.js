574ae9b9df4b54f69ff454d4f41262ad08c7d912
TITANFR <titanfrmax@gmail.com>
2025-02-01 11:41:40 +0100

    création page auth : connexion, inscription, compte, modif mot de passe

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/Pages/home.html"),
     new Route("/players", "Players", "Pages/players.html"),
+    new Route("/signin", "Connexion", "Pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "Pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "Pages/auth/account.html"),
+    new Route("/editpassword", "Changement de mot de passe", "Pages/auth/editpassword.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
